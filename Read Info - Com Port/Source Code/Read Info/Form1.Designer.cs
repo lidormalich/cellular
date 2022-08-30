4018c5be058e4fd0da743ec9eb16c51036e61da5
Lidor Repo <lidormalich@gmail.com>
2022-08-30 10:46:56 +0300

    add Source Code

---
+++ Read Info - Com Port/Source Code/Read Info/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace Read_Info
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.resultTXT = new System.Windows.Forms.RichTextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cBoxComPort = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.groupBox3.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(159, 450);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(286, 23);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 494);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 35);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "פרטי מכשיר";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // resultTXT
+            // 
+            this.resultTXT.Location = new System.Drawing.Point(24, 21);
+            this.resultTXT.Name = "resultTXT";
+            this.resultTXT.ReadOnly = true;
+            this.resultTXT.Size = new System.Drawing.Size(522, 266);
+            this.resultTXT.TabIndex = 7;
+            this.resultTXT.Text = "";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.resultTXT);
+            this.groupBox3.Location = new System.Drawing.Point(12, 114);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(570, 306);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "פלט";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cBoxComPort);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(409, 74);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Com Port";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(291, 22);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(118, 35);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "סגירת פורט";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Port";
+            // 
+            // cBoxComPort
+            // 
+            this.cBoxComPort.FormattingEnabled = true;
+            this.cBoxComPort.Location = new System.Drawing.Point(99, 28);
+            this.cBoxComPort.Name = "cBoxComPort";
+            this.cBoxComPort.Size = new System.Drawing.Size(175, 24);
+            this.cBoxComPort.TabIndex = 0;
+            this.cBoxComPort.DropDown += new System.EventHandler(this.cBoxComPort_DropDown);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(158, 495);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 35);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "מספר סיראלי";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(303, 495);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(140, 35);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "IMEI";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(449, 495);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(140, 35);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "פרטי תוכנה";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("David", 10.8F, System.Drawing.FontStyle.Bold);
+            this.label2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label2.Location = new System.Drawing.Point(217, 605);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(183, 19);
+            this.label2.TabIndex = 33;
+            this.label2.Text = "►♣Lidor Malich♣◄";
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.button6.Image = global::Read_Info.Properties.Resources.settings__1_;
+            this.button6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button6.Location = new System.Drawing.Point(303, 535);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(241, 57);
+            this.button6.TabIndex = 32;
+            this.button6.Text = "איפוס";
+            this.button6.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button7.Image = global::Read_Info.Properties.Resources.download__1_;
+            this.button7.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button7.Location = new System.Drawing.Point(56, 534);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(241, 57);
+            this.button7.TabIndex = 31;
+            this.button7.Text = "מצב הורדה";
+            this.button7.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 633);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox3);
+            this.Name = "Form1";
+            this.Text = "קריאת מידע מהמכשיר- לידור מליח";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox resultTXT;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cBoxComPort;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Read Info - Com Port/Source Code/Read Info/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Read_Info
+{
+    public partial class Form1 : Form
+    {
+        public string[] ports;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ClosePort();
+            sendComendToPort("AT+DEVCONINFO");
+            readruslt();
+        }
+
+        private void readruslt()
+        {
+            try
+            {
+                resultTXT.Text = serialPort1.ReadLine();
+            }
+            catch (TimeoutException)
+            {
+                resultTXT.Text = "Timeout Exception";
+            }
+            try
+            {
+                resultTXT.Text = serialPort1.ReadLine();
+            }
+            catch (TimeoutException)
+            {
+                resultTXT.Text = "Timeout Exception";
+            }
+        }
+
+        private void cBoxComPort_DropDown(object sender, EventArgs e)
+        {
+            scanPort();
+            //ListComPort();
+        }
+        private void ClosePort()
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+                progressBar1.Value = 0;
+            }
+        }
+        private void scanPort()
+        {
+            ports = SerialPort.GetPortNames();
+            cBoxComPort.Items.Clear();
+            cBoxComPort.Items.AddRange(ports);
+            //cBoxComPort.SelectedIndex = 0;
+        }
+        private void sendComendToPort(string commend)
+        {
+            //string portText = cBoxComPort.SelectedValue.ToString();
+
+            //if (portText == "Null")
+            //{
+            //    MessageBox.Show("לא נבחרה יציאה", "שגיאה");
+            //    return;
+            //}
+            try
+            {
+
+                progressBar1.Value = 0;
+                serialPort1.PortName = cBoxComPort.Text;
+                serialPort1.BaudRate = 115200;
+                serialPort1.ReadTimeout = 1500;
+                serialPort1.WriteTimeout = 1500;
+                progressBar1.Value = 50;
+                ClosePort();
+                serialPort1.Open();
+                serialPort1.WriteLine(commend); //dl mode
+                resultTXT.Text = serialPort1.ReadExisting();
+
+                progressBar1.Value = 100;
+
+
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            scanPort();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ClosePort();
+            sendComendToPort("AT+SERIALNO=1,0");
+            readruslt();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ClosePort();
+            sendComendToPort("AT+IMEINUM");
+            readruslt();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ClosePort();
+            sendComendToPort("AT+SWVER");
+            readruslt();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ClosePort();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            ClosePort();
+            sendComendToPort("AT+FUS?");
+            readruslt();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ClosePort();
+            DialogResult dialogResult = MessageBox.Show("האם אתה בטוח שאתה רוצה לאפס את המכשיר?", "אזהרת איפוס", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                sendComendToPort("AT+FACTORST=0,0");
+                readruslt();
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                //do something else
+            }
+            
+        }
+    }
+}
